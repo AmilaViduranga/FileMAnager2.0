403f66824505381cc34cbba5b452a4a6c885a6a8
Amila <amila@info-share.org>
2017-01-31 14:16:12 +0530

    add resource file view controller and its routes

--- app/routes/moduleRoutes/DisplayRoutes/ResourceViewRoute.js
+++ app/routes/moduleRoutes/DisplayRoutes/ResourceViewRoute.js
@@ -1,3 +1,22 @@
 /**
  * Created by amila on 1/31/17.
  */
+/*
+ * view the files
+ */
+
+var express = require('express');
+var router = express.Router();
+var ControllerMap = require('../../../Controller/ControllerMap');
+var ResourceViewController = ControllerMap.DisplayController.ResourcesViewController;
+
+/*
+ * get the files requested by client
+ */
+router.get('/:token/:res_id/:file', function(req, res, next) {
+    var token = req.params.token;
+    var res_id = req.params.res_id;
+    return ResourceViewController.getResource(token, res_id, res);
+})
+
+module.exports = router;
