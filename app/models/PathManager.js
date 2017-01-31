be9b6bb06e217ddc470b30d22ab7848fcfea3bd0
Amila <amila@info-share.org>
2017-01-31 14:17:15 +0530

    add some path locations(for resource management)

--- app/models/PathManager.js
+++ app/models/PathManager.js
@@ -5,7 +5,13 @@ var FilePath  = {
     profile_images: 'Files/Profile_images/',
     snap: 'Files/snap/',
     resoursces :{
-        video:'Files/Resoursces/Video',
+        video:'Files/Resoursces/videos/',
+        presentation: 'Files/Resources/presentations/',
+        animation: 'Files/Resources/animations/',
+        knowledge_nuget: 'Files/Resources/knowledge_nuggets/',
+        revision_note: 'Files/Resources/revision_notes/',
+        worksheet: 'Files/Resources/worksheets/',
+        interactive_activities: 'Files/Resources/interactive_activities/'
     }
 };
 
