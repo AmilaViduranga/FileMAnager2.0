f4d36ab1404e27d2be9e29e402ab896614ee7b58
AmilaViduranga <amilavidurangawelikala@gmail.com>
2017-02-10 15:00:56 +0530

    change the routs for handle request profile image and tumbline

--- app/routes/moduleRoutes/ProfileImagesViewRoute.js
+++ app/routes/moduleRoutes/ProfileImagesViewRoute.js
@@ -17,16 +17,21 @@ var ProfileImageUploadController = ControllerMap.UploadsController.ProfileImageU
  * get the profile image of user
  */
 router.get('/:token', function (req, res, next) {
-    ProfileImageViewController.getImageUser(req.params.token, res);
+    ProfileImageViewController.getImageUser(req.params.token, "profile", res);
 });
 
+/*
+ * get the tubline of profile image
+ */
+router.get('/tumbline/:token', function(req, res, next) {
+    ProfileImageViewController.getImageUser(req.params.token, "profile_tubline",res);
+})
 
 /*
  * post profile image file to file manager and db
  * */
 
 router.post('/', function (req, res, next) {
-
     var sampleFile = req.files.sampleFile;
     var token = req.body.token;
     ProfileImageUploadController.getUser(token, sampleFile, res);
